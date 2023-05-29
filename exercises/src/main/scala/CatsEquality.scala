e324b874ba832a52d390543d3a761d3211b5b446
AlekseiBulaev <bulaev.a.u@gmail.com>
2023-05-28 20:07:55 -0400

    1.5.5 Exercise: Equality, Liberty, and Felinity

---
+++ exercises/src/main/scala/CatsEquality.scala
@@ -0,0 +1,22 @@
+
+object CatsEquality {
+  final case class Cat(name: String, age: Int, color: String)
+
+  import cats.Eq
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.eq._
+
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat]((cat1, cat2) => {
+    cat1.name === cat2.name && cat1.age === cat2.age && cat1.color === cat2.color
+  })
+
+  import cats.instances.option._
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+  println(cat1 === cat2)
+  println(optionCat1 === optionCat2)
+}
