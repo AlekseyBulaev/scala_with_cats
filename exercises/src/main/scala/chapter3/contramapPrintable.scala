7847e46b2574d1b38b175db1dce4c41430849e30
AlekseiBulaev <bulaev.a.u@gmail.com>
2023-05-29 20:51:55 -0400

    3.6.1.1 Exercise: Showing off with Contramap

--- exercises/src/main/scala/chapter3/contramapPrintable.scala
+++ exercises/src/main/scala/chapter3/contramapPrintable.scala
@@ -1,5 +1,7 @@
 package chapter3
 
+import chapter2.Printable
+
 object contramapPrintable {
   trait Printable[A] { self =>
     def format(value: A): String
@@ -11,5 +13,28 @@ object contramapPrintable {
   }
   def format[A](value: A)(implicit printable: Printable[A]): String = {
     printable.format(value)
+  }
+
+  final case class Box[A](value: A)
+
+  implicit val stringPrintable: Printable[String] =
+    new Printable[String] {
+      def format(value: String): String =
+        s"'${value}'"
+    }
+  implicit val booleanPrintable: Printable[Boolean] =
+    new Printable[Boolean] {
+      def format(value: Boolean): String =
+        if(value) "yes" else "no"
+    }
+  implicit def boxStringPrintable: Printable[Box[String]] = stringPrintable.contramap(x => x.value)
+  implicit def boxBooleanPrintable: Printable[Box[Boolean]] = booleanPrintable.contramap(x => x.value)
+
+  implicit def boxPrintable[A](implicit p: Printable[A]): Printable[Box[A]] = p.contramap[Box[A]](_.value)
+  def main(input: Array[String]): Unit = {
+    format(Box("hello world"))
+
+    format(Box(true))
+//      format(Box(123))
   }
 }
