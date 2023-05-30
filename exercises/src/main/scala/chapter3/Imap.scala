b6f42171a4b5e232c570271164b75c8abe190447
AlekseiBulaev <bulaev.a.u@gmail.com>
2023-05-29 21:04:51 -0400

    3.6.2.1 Transformative Thinking with imap

---
+++ exercises/src/main/scala/chapter3/Imap.scala
@@ -0,0 +1,12 @@
+package chapter3
+
+object Imap {
+  trait Codec[A] { self =>
+    def encode(value: A): String
+    def decode(value: String): A
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+      override def encode(value: B): String = self.encode(enc(value))
+      override def decode(value: String): B = dec(self.decode(value))
+    }
+  }
+}
