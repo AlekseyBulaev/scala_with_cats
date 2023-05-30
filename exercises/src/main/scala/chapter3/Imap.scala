bac2637d2c6ea00470b5995670c9cb55efec4a9e
AlekseiBulaev <bulaev.a.u@gmail.com>
2023-05-29 21:25:09 -0400

    Transformative Thinking with imap Part 3

--- exercises/src/main/scala/chapter3/Imap.scala
+++ exercises/src/main/scala/chapter3/Imap.scala
@@ -9,4 +9,33 @@ object Imap {
       override def decode(value: String): B = dec(self.decode(value))
     }
   }
+  def encode[A](value: A)(implicit c: Codec[A]): String =
+    c.encode(value)
+
+  def decode[A](value: String)(implicit c: Codec[A]): A =
+    c.decode(value)
+
+  implicit val stringCodec: Codec[String] =
+    new Codec[String] {
+      def encode(value: String): String = value
+
+      def decode(value: String): String = value
+    }
+
+  final case class Box[A](value: A)
+
+  def main(in: Array[String]): Unit = {
+    implicit val doubleCodec: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
+
+    implicit def boxCodec[A](implicit p: Codec[A]): Codec[Box[A]] = p.imap(Box(_), _.value)
+
+    println(encode(123.4))
+    // res11: String = "123.4"
+    println(decode[Double]("123.4"))
+    // res12: Double = 123.4
+    println(encode(Box(123.4)))
+    // res13: String = "123.4"
+    println(decode[Box[Double]]("123.4"))
+    // res14: Box[Double] = Box(123.4)
+  }
 }
